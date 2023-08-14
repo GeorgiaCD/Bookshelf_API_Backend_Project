a07f492fe7774dc754d8e41480025ab43d63ba58
Kibiko <kevinjunchan@gmail.com>
2023-08-14 16:11:20 +0200

    bookDTO complete

--- src/main/java/models/BookDTO.java
+++ src/main/java/models/BookDTO.java
@@ -1,4 +1,74 @@
 package models;
 
+import java.util.List;
+
 public class BookDTO {
+
+    private String title;
+    private Long authorId;
+    private Long genreId;
+    private List<Long> bookshelfIds;
+    private int yearPublished;
+    private int numberOfPages;
+
+    public BookDTO(String title, Long authorId, Long genreId, List<Long> bookshelfIds, int yearPublished, int numberOfPages) {
+        this.title = title;
+        this.authorId = authorId;
+        this.genreId = genreId;
+        this.bookshelfIds = bookshelfIds;
+        this.yearPublished = yearPublished;
+        this.numberOfPages = numberOfPages;
+    }
+
+    public BookDTO(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public Long getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(Long genreId) {
+        this.genreId = genreId;
+    }
+
+    public List<Long> getBookshelfIds() {
+        return bookshelfIds;
+    }
+
+    public void setBookshelfIds(List<Long> bookshelfIds) {
+        this.bookshelfIds = bookshelfIds;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
 }
