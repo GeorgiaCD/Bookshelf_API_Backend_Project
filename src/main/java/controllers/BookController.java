102e0405e6c1415854245ffef2cedb9e170cbb68
Francesca Marshall <francescam3105@gmail.com>
2023-08-15 10:48:49 +0100

    bookController mappings written

--- src/main/java/controllers/BookController.java
+++ src/main/java/controllers/BookController.java
@@ -1,4 +1,61 @@
 package controllers;
 
+import models.Book;
+import models.BookDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.BookService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.RecursiveTask;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    BookService bookService;
+
+//    Handles following:
+//    * GET /books
+//    * GET /books?genre="cooking"
+//    INDEX
+    @GetMapping
+    public ResponseEntity<List<Book>> getALlBooks(){
+        List<Book> allBooks = bookService.getAllBooks();
+        return new ResponseEntity<>(allBooks, HttpStatus.OK);
+    }
+
+//    SHOW
+    @GetMapping(value = "/{id")
+    public ResponseEntity<Optional<Book>> getBook(@PathVariable Long id){
+        Book foundBook = bookService.findBookById(id);
+        return new ResponseEntity(foundBook, HttpStatus.OK);
+    }
+
+//    CREATE
+    @PostMapping
+    public ResponseEntity<List<Book>> addBook(@RequestBody BookDTO bookDTO){
+        bookService.saveBook(bookDTO);
+        return new RequestBody(bookService.getAllBooks(), HttpStatus.CREATED);
+    }
+
+//    UPDATE
+    @PutMapping(value = "/{id")
+    public ResponseEntity<Book> updateBook(@RequestBody BookDTO bookDTO, @PathVariable Long id){
+        Book updatedBook = bookService.updateBook(bookDTO, id);
+        return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+    }
+
+    //    DELETE
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+
 }
