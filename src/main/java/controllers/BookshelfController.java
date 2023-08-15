002ae92a0b820ab502210e5b9e39e721e82821e4
rohaib1 <rohaib192@gmail.com>
2023-08-15 10:24:47 +0100

    BookshelfController complete

--- src/main/java/controllers/BookshelfController.java
+++ src/main/java/controllers/BookshelfController.java
@@ -1,4 +1,40 @@
 package controllers;
 
+import models.Bookshelf;
+import models.BookshelfDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.BookshelfService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "bookshelf")
 public class BookshelfController {
+
+    @Autowired
+    BookshelfService bookshelfService;
+
+    @PostMapping
+    public ResponseEntity<Bookshelf> addToBookshelf(BookshelfDTO bookshelfDTO){
+        Bookshelf bookshelf = bookshelfService.addToBookshelf(bookshelfDTO);
+        return new ResponseEntity<>(bookshelf, HttpStatus.OK);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Bookshelf> updateBookshelf(BookshelfDTO bookshelfDTO, @PathVariable Long id){
+        Bookshelf bookshelf = bookshelfService.updateBookshelf(bookshelfDTO);
+        return new ResponseEntity<>(bookshelf, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Bookshelf> deleteBookFromBookshelf(BookshelfDTO bookshelfDTO, @PathVariable Long id){
+        Bookshelf bookshelf = bookshelfService.deleteBookFromBookshelf(bookshelfDTO);
+        return new ResponseEntity<>(bookshelf, HttpStatus.OK);
+    }
+
+
+
 }
